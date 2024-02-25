655983d6014318d5f4043316a29c1e82eaab6672
AlexLuers <156149375+AlexLuers@users.noreply.github.com>
2024-02-25 00:51:26 -0500

    Update GetrLoopy.java

--- GetrLoopy/src/getrloopy/GetrLoopy.java
+++ GetrLoopy/src/getrloopy/GetrLoopy.java
@@ -14,23 +14,25 @@ public class GetrLoopy {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         Scanner in = new Scanner(System.in);
-         
+         //counting uo to 30         
          for(int r=0; r < 31; r++)
          {
              System.out.println("" + r);
          }
          System.out.println("----------");
+         //counting down from 30
          for(int r = 30; r > 0; r--)
          {
              System.out.println("" + r);
          }
          System.out.println("----------");
+         //counting to 18 by 3
          for(int r=0; r < 19; r+= 3)   
          {
              System.out.println("" + r);
          }
          System.out.println("----------");
+         //counting down from 10 by 2
          for(int r=10; r > -1; r-=2)
          {
              System.out.println("" + r);
